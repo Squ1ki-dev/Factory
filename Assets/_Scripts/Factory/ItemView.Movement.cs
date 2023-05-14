94b10c90ffdc8f50e3e34cdae3f2114887897295
xMoRoZoKx <105913669+xMoRoZoKx@users.noreply.github.com>
2023-05-15 01:27:35 +0300

    fix anim speed

--- Assets/_Scripts/Factory/ItemView.Movement.cs
+++ Assets/_Scripts/Factory/ItemView.Movement.cs
@@ -8,11 +8,11 @@ public partial class ItemView : MonoBehaviour
 {
     public UnityEvent onEndMove = new UnityEvent();
     public float moveDuration { get; private set; } = 0.5f;
-    
+
     public void LocalMoveTo(Vector3 pos, Vector3 rotation)
     {
         transform.DOKill();
-        transform.DOLocalRotate(rotation, moveDuration);
+        transform.DOLocalRotate(rotation, moveDuration / 2);
         transform.DOLocalMove(pos, moveDuration * 0.5f).OnComplete(() => transform.DOLocalMove(pos, moveDuration * 0.5f)).OnComplete(() => onEndMove?.Invoke());
     }
 }
